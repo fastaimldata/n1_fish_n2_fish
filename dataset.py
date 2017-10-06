7a6c40b38ebd275de938a098c332bd2b096ea42d
Dmytro Poplavskiy <dmytro.poplavskiy@gmail.com>
2017-10-06 19:36:09 +1000

    implemented ruler detection and ssd on rotated crops

---
+++ dataset.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import pickle
+import os
+
+VIDEOS_DIR = '../input/train_videos'
+IMAGES_DIR = '../input/train_videos/img'
+MASKS_DIR = '../output/ruler_masks'
+AVG_MASKS_DIR = '../output/ruler_masks_avg'
+RULER_CROPS_DIR = '../output/ruler_crops'
+
+SPECIES = ['fourspot', 'grey sole', 'other', 'plaice', 'summer', 'windowpane', 'winter']
+CLASSES = ['_'] + SPECIES
+
+ASPECT_RATIO_TABLE = {
+    # '_': 0.1,
+    'fourspot': 0.55,
+    'grey sole': 0.55,
+    'other': 0.4,
+    'plaice': 0.55,
+    'summer': 0.5,
+    'windowpane': 0.55,
+    'winter': 0.5
+}
+
+
+def video_clips() -> {str}:
+    cache_fn = '../output/video_clips.pkl'
+    try:
+        clips = pickle.load(open(cache_fn, 'rb'))
+    except FileNotFoundError:
+        clips = {}
+        for dir_name in os.listdir(IMAGES_DIR):
+            clip_dir = os.path.join(IMAGES_DIR, dir_name)
+            frames = []
+            for frame_name in os.listdir(clip_dir):
+                if not frame_name.endswith('.jpg'):
+                    continue
+                frames.append(frame_name[:-len('.jpg')])
+            clips[dir_name] = frames
+
+        pickle.dump(clips, open(cache_fn, 'wb'))
+    return clips
+
+
+def image_fn(clip_name, frame):
+    return '{}/{}/{:04}.jpg'.format(IMAGES_DIR, clip_name, int(frame)+1)
+
+
+def image_crop_fn(clip_name, frame):
+    return '{}/{}/{:04}.jpg'.format(RULER_CROPS_DIR, clip_name, int(frame)+1)
---
+++ fish_detection.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+import json
+import os
+import pickle
+from typing import List, Dict
+from collections import namedtuple
+from skimage.transform import SimilarityTransform
+from sklearn.model_selection import train_test_split
+import dataset
+from dataset import SPECIES, CLASSES
+
+INPUT_ROWS = 720
+INPUT_COLS = 360
+input_shape = (INPUT_ROWS, INPUT_COLS, 3)
+NUM_CLASSES = len(CLASSES)
+
+FishDetection = namedtuple('FishDetection', ['clip_name', 'frame', 'fish_number', 'x1', 'y1', 'x2', 'y2', 'class_id'])
+RulerPoints = namedtuple('RulerPoints', ['x1', 'y1', 'x2', 'y2'])
+
+
+class FishDetectionDataset:
+    def __init__(self):
+        self.video_clips = dataset.video_clips()
+
+        cache_fn = '../output/fish_detection.pkl'
+        try:
+            # raise FileNotFoundError
+            self.detections = pickle.load(open(cache_fn, 'rb'))  # type: Dict[FishDetection]
+        except FileNotFoundError:
+            self.detections = self.load()  # type: Dict[FishDetection]
+            pickle.dump(self.detections, open(cache_fn, 'wb'))
+
+        self.train_clips, self.test_clips = train_test_split(sorted(self.detections.keys()),
+                                                             test_size=0.1,
+                                                             random_state=42)
+
+        self.nb_train_samples = sum([len(self.detections[clip]) for clip in self.train_clips])
+        self.nb_test_samples = sum([len(self.detections[clip]) for clip in self.test_clips])
+
+        self.ruler_points = {}
+        ruler_points = pd.read_csv('../output/ruler_points.csv')
+        for _, row in ruler_points.iterrows():
+            self.ruler_points[row.clip_name] = RulerPoints(x1=row.ruler_x0, y1=row.ruler_y0, x2=row.ruler_x1, y2=row.ruler_y1)
+
+    def load(self):
+        detections = {}
+        ds = pd.read_csv('../input/N1_fish_N2_fish_-_Training_set_annotations.csv')
+
+        species = ds.as_matrix(columns=['species_'+s for s in SPECIES])
+        cls_column = np.argmax(species, axis=1)+1
+        cls_column[np.max(species, axis=1) == 0] = 0
+
+        for row_id, row in ds.iterrows():
+            clip_name = row.video_id
+            if clip_name not in detections:
+                detections[clip_name] = []
+
+            detections[clip_name].append(
+                FishDetection(
+                    clip_name=clip_name,
+                    frame=row.frame,
+                    fish_number=row.fish_number,
+                    x1=row.x1, y1=row.y1,
+                    x2=row.x2, y2=row.y2,
+                    class_id=int(cls_column[row_id])
+                )
+            )
+        return detections
+
+    def transform_for_clip(self, clip_name, dst_w, dst_h, points_random_shift=0):
+        points = self.ruler_points[clip_name]
+
+        ruler_points = np.array([[points.x1, points.y1], [points.x2, points.y2]])
+        img_points = np.array([[dst_w * 0.1, dst_h / 2], [dst_w * 0.9, dst_h / 2]])
+
+        if points_random_shift > 0:
+            img_points += np.random.uniform(-points_random_shift, points_random_shift, (2, 2))
+
+        tform = SimilarityTransform()
+        tform.estimate(dst=ruler_points, src=img_points)
+
+        return tform
